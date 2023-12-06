c697afe2f0570bbeafbf903f83bf4c017dac3bc7
Dawid <dburda@student.agh.edu.pl>
2023-12-06 22:20:26 +0100

    Login, register in progress

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,30 @@
 <template>
-  <router-view />
+<v-app>
+  <v-app-bar>
+    <v-app-bar-title>
+      <Header></Header>
+    </v-app-bar-title> 
+
+    <HeaderButtons></HeaderButtons>
+    
+  
+  </v-app-bar>
+  <v-main>
+    <router-view />
+  </v-main>
+</v-app>
 </template>
 
-<script lang="ts" setup>
-  //
+<script lang="ts">
+import Header from "./components/Header.vue";
+import HeaderButtons from "./components/HeaderButtons.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    HeaderButtons
+  }
+}
+
 </script>
---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script lang="ts">
+
+export default {
+  data() {
+    return {}
+  },
+
+  methods: {
+
+  },
+};
+</script>
