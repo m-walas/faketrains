a5cce095026a1ff0b67dd6a3f26f82490882db80
mwalas <matwal777@gmail.com>
2023-12-14 01:46:53 +0100

    Update deployment script and header title

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     
     <v-sheet class="d-flex justify-space-between align-center " width="100%">
-      <div class="text-h4 ml-3">FakeTrains v2</div>
+      <div class="text-h4 ml-3">FakeTrains</div>
     </v-sheet>
 </template>
 
